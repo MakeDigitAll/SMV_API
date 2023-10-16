95a45341b17938f28b73b9955ee308a3dc82700c
MakeDigitAll <makers@makedigitall.com>
2023-10-15 22:33:31 -0600

    SMV Migrate HTTP to HTTPS

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
 
-const davidRoutes = require('./routes/routesDavid');
-const microservCompras = require('./routes/routesCompras');
-const microserVentas = require('./routes/routesVentas');
-const user = require('./routes/login/login');
+const davidRoutes = require("./routes/routesDavid");
+const microservCompras = require("./routes/routesCompras");
+const microserVentas = require("./routes/routesVentas");
+const user = require("./routes/login/login");
+
+const keyPath = path.join(__dirname, "controllers", "Security", "key.pem");
+const certPath = path.join(__dirname, "controllers", "Security", "cert.pem");
+
+const options = {
+  key: fs.readFileSync(keyPath),
+  cert: fs.readFileSync(certPath),
+};
+
 const app = express();
-const chat = require('./routes/chat/chatRoutes');
+const chat = require("./routes/chat/chatRoutes");
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(davidRoutes);
@@ -19,10 +31,21 @@ app.use(user);
 app.use(chat);
 
 app.use((err, req, res, next) => {
-    return res.json({
-        message: err.message
-    })
-})
-const port = process.env.PORT || 4000;
-app.listen(port);
-console.log("servidor en el puerto " + port);
+  return res.json({
+    message: err.message,
+  });
+});
+// Rutas de tu aplicación Express
+app.get("/", (req, res) => {
+  res.send("Servidor Express ok");
+});
+
+// Configuración de tus rutas adicionales
+
+const httpsServer = https.createServer(options, app);
+
+const PORT = 443;
+
+httpsServer.listen(PORT, () => {
+  console.log("Servidor Express ejecución en el puerto 443");
+});
